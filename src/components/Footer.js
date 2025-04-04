3f54eff7b5898e78978137429f6b1eb2f01a886e
Eric Hoover <ehoover3@yahoo.com>
2025-04-04 15:09:21 -0400

    refactor components into smaller files

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <a href='https://erichoover.org' target='_blank' rel='noopener noreferrer'>
+        Return to erichoover.org
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
